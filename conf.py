4c312ff23c0827dee77df08e6164ce65947a7a3a
plsharevme <64370408+plsharevme@users.noreply.github.com>
2021-12-05 16:57:54 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "plsharevme/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
